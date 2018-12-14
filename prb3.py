f228de793e285ad83eb92c4abd101aa626e0d2c0
tejas01101001 <tejas18@iitg.ac.in>
2018-12-14 12:06:01 +0530

    THIRD PRB

---
+++ prb3.py
@@ -0,0 +1,30 @@
+def gcd(a,b):
+#using recursion
+	if b==0:
+		return a
+	return gcd(b,a%b)
+
+def hcf(a,b):
+#using while loop	
+	while a!=b
+		if a>b:
+			a-=b
+		else:
+			b-=a
+	return a		
+			
+
+
+
+
+	
+
+
+
+
+
+
+
+
+		
+
